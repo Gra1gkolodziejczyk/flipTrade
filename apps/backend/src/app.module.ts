d0a7d3b5178e6bec4b67864d6044bcdfd3872985
Gra1gKolodziejczyk <graig.kolodziejczyk@ynov.com>
2025-06-13 16:28:01 +0200

    resolv conflict

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -3,10 +3,11 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { AuthController } from './auth/auth.controller';
 import { PrismaModule } from './prisma/prisma.module';
+import { UserController } from './user/user.controller';
 
 @Module({
   imports: [AuthModule, UserModule, PrismaModule],
-  controllers: [AuthController],
+  controllers: [AuthController, UserController],
   providers: [],
 })
 export class AppModule {}
--- apps/backend/src/user/user.module.ts
+++ apps/backend/src/user/user.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { UserService } from './user.service';
+import { UserController } from './user.controller';
 
-@Module({})
+@Module({
+  imports: [PrismaModule],
+  controllers: [UserController],
+  providers: [UserService, PrismaService],
+})
 export class UserModule {}
