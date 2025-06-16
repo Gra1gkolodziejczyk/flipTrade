f546fb3661492839dbe5e796982a8dd8fdbbc557
Gra1gKolodziejczyk <graig.kolodziejczyk@ynov.com>
2025-06-16 10:43:11 +0200

    add folder for trades - module controlle service

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { AuthController } from './auth/auth.controller';
 import { PrismaModule } from './prisma/prisma.module';
+import { TradeModule } from './trade/trade.module';
 
 @Module({
-  imports: [AuthModule, UserModule, PrismaModule],
+  imports: [AuthModule, UserModule, PrismaModule, TradeModule],
   controllers: [AuthController],
   providers: [],
 })
